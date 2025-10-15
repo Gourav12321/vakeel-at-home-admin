c41e286a953fcd66fbab3d43c7133dd8a87839db
Shobhit Nautiyal <shobhit.nautiyal99@gmail.com>
2025-10-15 20:43:59 +0530

    Add statistics endpoint to API and implement data fetching in Dashboard

--- src/apis/index.js
+++ src/apis/index.js
@@ -3,5 +3,6 @@ export const apiBaseUrl = "http://localhost:5970/api/v1";
 export const apiUrls = {
   auth: {
     login: "/auth/",
+    statistics: "/auth/statistics",
   },
 };
--- src/app/(main)/admin/dashboard/page.jsx
+++ src/app/(main)/admin/dashboard/page.jsx
@@ -1,7 +1,26 @@
+"use client";
+
 import Title from "@/components/Title/Title";
-import React from "react";
+import useGetQuery from "@/hooks/getQuery.hook";
+
+import { apiUrls } from "@/apis";
+import { useEffect } from "react";
 
 const Dashboard = () => {
+  const { getQuery } = useGetQuery();
+
+  useEffect(() => {
+    getQuery({
+      url: apiUrls.auth.statistics,
+      onSuccess: (res) => {
+        console.log(res);
+      },
+      onFail: (err) => {
+        console.log(err);
+      },
+    });
+  }, []);
+
   return (
     <div className="text-slate-950">
       <Title title={"Dashboard"} />
